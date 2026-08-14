89ae9a484395411c2697c3652b5d0bdbd8dfa897
Peter S <peterpreneur@gmail.com>
2026-08-14 17:06:25 -0500

    addTwoNumbers

---
+++ leetcode/src/main/java/org/peterpreneur/AddTwoNumbers.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.peterpreneur;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author peter
+ */
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        //2 non-empty linked list containing two non-negative integers
+        //digits are stored in reverse order
+        //each nodes contains a single digit
+        //add the 2 numbers and return the sum as linked list
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        addTwoNumbers(l1, l2);
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+    
+
+    }
+
+}
