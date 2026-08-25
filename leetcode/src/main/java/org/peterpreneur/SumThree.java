b62ff4f4dc299e06790c1ebe544a62328f017f18
Peter S <peterpreneur@gmail.com>
2026-08-25 18:33:01 -0500

    threesum

--- leetcode/src/main/java/org/peterpreneur/SumThree.java
+++ leetcode/src/main/java/org/peterpreneur/SumThree.java
@@ -17,59 +17,64 @@ public class SumThree {
     public static void main(String[] args) {
         // int[] nums = {1, 2, -2, -1};
         // int[] nums = {-1,0,1,2,-1,-4};
-        int[] nums = {-2,0,1,1,2};
+        int[] nums = {-2, 0, 1, 1, 2};
         // int[] nums = {2, -3, 0, -2, -5, -5, -4, 1, 2, -2, 2, 0, 2, -4, 5, 5, -10};
         System.out.println(threeSum(nums).toString());
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
         if (nums.length < 3 || nums.length > 3000) {
-            return new ArrayList<>();
+            return result;
         }
 
         //sort the array first to avoid duplicates
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        // System.out.println(Arrays.toString(nums));
 
-        List<List<Integer>> list1 = new ArrayList<>();
-
-        int arrayLen = nums.length;
-
-        for (int i = 0; i < arrayLen; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             // System.out.println("i = " + nums[i] + " ---");
 
-            for (int l = i + 1; l < arrayLen; l++) {
-                // System.out.println("l = " + nums[l]);
+            // Do not reuse the same first value.
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-                for (int r = arrayLen - 1; r > l; r--) {
+            // The remaining values are also positive, so zero is impossible.
+            if (nums[i] > 0) {
+                break;
+            }
 
-                    if ((i != l) && (l != r) && (r != i)) {
-                        System.out.println("i=" + i + " l=" + l + " r=" + r);
+            int l = i + 1;
+            int r = nums.length - 1;
 
-                        int total = nums[i] + nums[l] + nums[r];
-                        if (total == 0) {
+            while (l < r) {
 
-                            List<Integer> list2 = new ArrayList<>();
-                            list2.add(nums[i]);
-                            list2.add(nums[l]);
-                            list2.add(nums[r]);
+                int total = nums[i] + nums[l] + nums[r];
+                if (total == 0) {
+                    result.add(List.of(nums[i], nums[l], nums[r]));
 
-                            list1.add(list2);
+                    l++;
+                    r--;
 
-                            break;
-                        }
+                    // Skip repeated left and right values.
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
                     }
+
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+
+                } else if (total < 0) {
+                    l++;
+                } else {
+                    r--;
                 }
             }
         }
-        if (list1.isEmpty()) {
-            return new ArrayList<>();
-        } else {
-            return list1.stream()
-                    .map(group -> group.stream().sorted().toList())
-                    .distinct()
-                    .toList();
-        }
+        return result;
     }
-
 }
+
