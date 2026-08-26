0c0b2c13643f3ae78a2043034e7b0d8874bac317
Peter S <peterpreneur@gmail.com>
2026-08-25 21:47:35 -0500

    test

--- leetcode/src/main/java/org/peterpreneur/ThreeSumClosest.java
+++ leetcode/src/main/java/org/peterpreneur/ThreeSumClosest.java
@@ -4,6 +4,8 @@
  */
 package org.peterpreneur;
 
+import java.util.Arrays;
+
 /**
  *
  * @author peter
@@ -13,34 +15,53 @@ public class ThreeSumClosest {
     public static void main(String[] args) {
 
         int[] nums = {-1, 2, 1, -4};
-
         int target = 1;
+
         System.out.println(threeSumClosest(nums, target));
     }
 
     public static int threeSumClosest(int[] nums, int target) {
-        if (nums.length < 3 || nums.length > 500) {
-            throw new CustomExceptions("Wrong length.");
+        if (nums == null || nums.length < 3 || nums.length > 500) {
+            throw new CustomExceptions("Array length must be between 3 and 500.");
         }
 
-        if (target < -Math.pow(10, 4) || target > Math.pow(10, 4)) {
-            throw new CustomExceptions("Invalid target.");
+        if (target < -10_000 || target > 10_000) {
+            throw new CustomExceptions("Target must be between -10,000 and 10,000.");
         }
 
-        int result = 0;
+        for (int number : nums) {
+            if (number < -1_000 || number > 1_000) {
+                throw new CustomExceptions("Each number must be between -1,000 and 1,000.");
+            }
+        }
+
+        Arrays.sort(nums);
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
         for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i] < -1000 || nums[i] > 1000) {
-                throw new CustomExceptions("Invalid value.");
-            }
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            int total = nums[i] + nums[i + 1] + nums[i + 2];
-            int diff = Math.abs(total - target);
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
 
-            if (i == 0 || diff < result) {
-                result = diff;
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+                if (currentSum == target) {
+                    return currentSum;
+                }
+
+                if (currentSum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
 
-        return result;
+        return closestSum;
     }
 }
