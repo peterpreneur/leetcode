bbeb23a65d230cdadf322506aecfb7fef0110fbc
Peter S <peterpreneur@gmail.com>
2026-08-25 21:37:08 -0500

    test

--- leetcode/src/main/java/org/peterpreneur/ThreeSumClosest.java
+++ leetcode/src/main/java/org/peterpreneur/ThreeSumClosest.java
@@ -12,8 +12,7 @@ public class ThreeSumClosest {
 
     public static void main(String[] args) {
 
-        // int[] nums = {-1, 2, 1, -4};
-        int[] nums = {-1, 2};
+        int[] nums = {-1, 2, 1, -4};
 
         int target = 1;
         System.out.println(threeSumClosest(nums, target));
@@ -24,15 +23,24 @@ public class ThreeSumClosest {
             throw new CustomExceptions("Wrong length.");
         }
 
-        if (nums[i] < -1000 || nums[i] > 1000) {
-            throw new CustomExceptions("Invalid value.");
-        }
-
-        if (target < -Math.pow(10, 4) || nums[i] > Math.pow(10, 4)) {
+        if (target < -Math.pow(10, 4) || target > Math.pow(10, 4)) {
             throw new CustomExceptions("Invalid target.");
         }
 
-        return 1;
+        int result = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] < -1000 || nums[i] > 1000) {
+                throw new CustomExceptions("Invalid value.");
+            }
 
+            int total = nums[i] + nums[i + 1] + nums[i + 2];
+            int diff = Math.abs(total - target);
+
+            if (i == 0 || diff < result) {
+                result = diff;
+            }
+        }
+
+        return result;
     }
 }
