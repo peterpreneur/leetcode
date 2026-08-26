e778bff9a82e6f14988c5c88f320ad4128a2521a
Peter S <peterpreneur@gmail.com>
2026-08-25 19:36:26 -0500

    3sumclosest

---
+++ leetcode/src/main/java/org/peterpreneur/ThreeSumClosest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.peterpreneur;
+
+/**
+ *
+ * @author peter
+ */
+public class ThreeSumClosest {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        return 1;
+
+    }
+}
