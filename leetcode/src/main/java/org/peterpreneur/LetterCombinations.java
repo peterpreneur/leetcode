72c9fc39027bee30a6066330887582724ce52e0e
Peter S <peterpreneur@gmail.com>
2026-08-25 22:02:55 -0500

    test

---
+++ leetcode/src/main/java/org/peterpreneur/LetterCombinations.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.peterpreneur;
+
+import java.util.List;
+
+/**
+ *
+ * @author peter
+ */
+public class LetterCombinations {
+
+    public static void main(String[] args) {
+        String input = "asdklasfjdflkjas";
+
+        System.out.println(letterCombinations(input));
+
+    }
+
+    private static List<String> letterCombinations(String digits) {
+        
+        return null;
+    }
+
+}
