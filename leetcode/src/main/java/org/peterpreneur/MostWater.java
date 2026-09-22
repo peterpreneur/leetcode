986cc1989a7704a7af86ca972bcf3f18f09eca46
Peter S <peterpreneur@gmail.com>
2026-09-22 11:46:19 -0500

    init

--- leetcode/src/main/java/org/peterpreneur/MostWater.java
+++ leetcode/src/main/java/org/peterpreneur/MostWater.java
@@ -11,19 +11,30 @@ package org.peterpreneur;
 public class MostWater {
 
     public static void main(String[] args) {
-        int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         System.out.println(maxArea(height));
     }
 
     public static int maxArea(int[] height) {
-        int n = height.length;
-        for (int i = 0; i < n; i++) {
-            int max = i * height[i];
-            System.out.println(max);
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
 
+        while (left < right) {
+            int width = right - left;
+            int shorterHeight = Math.min(height[left], height[right]);
+            int currentArea = width * shorterHeight;
+
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
         }
-
-        return 1;
+        
+        return maxArea;
     }
 
 }
